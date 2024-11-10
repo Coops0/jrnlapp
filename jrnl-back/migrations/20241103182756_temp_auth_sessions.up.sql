e9ce1117109da989364321f9e56297e74754b3a0
Coops0 <51346812+Coops0@users.noreply.github.com>
2024-11-09 20:48:35 -0500

    Update 20241103182756_temp_auth_sessions.up.sql

--- jrnl-back/migrations/20241103182756_temp_auth_sessions.up.sql
+++ jrnl-back/migrations/20241103182756_temp_auth_sessions.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS temp_auth_sessions
 (
-    csrf_token    TEXT PRIMARY KEY NOT NULL,
-    nonce TEXT             NOT NULL,
-    expires_at    TIMESTAMPTZ      NOT NULL
+    csrf_token TEXT PRIMARY KEY NOT NULL,
+    nonce      TEXT             NOT NULL,
+    expires_at TIMESTAMPTZ      NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS temp_auth_sessions_expires_at_idx ON temp_auth_sessions (expires_at);
