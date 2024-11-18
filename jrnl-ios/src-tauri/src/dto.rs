f33407eb6aed8cabf4b58d55750fdd8effb4724b
Coops0 <51346812+Coops0@users.noreply.github.com>
2024-11-18 16:12:19 -0500

    ill mean smth to u

--- jrnl-ios/src-tauri/src/dto.rs
+++ jrnl-ios/src-tauri/src/dto.rs
@@ -7,4 +7,7 @@ pub struct Entry {
     pub date: NaiveDate,
     pub emotion_scale: f32,
     pub text: Option<String>,
+    #[serde(default)]
+    pub saved: bool,
+    pub id: String,
 }
--- jrnl-ios/src-tauri/src/lib.rs
+++ jrnl-ios/src-tauri/src/lib.rs
@@ -1,6 +1,5 @@
 use crate::dto::Entry;
 use crate::error::{JrnlIosError, JrnlIosResult};
-use chrono::Local;
 use tauri_plugin_fs::FsExt;
 
 mod context;
@@ -17,70 +16,42 @@ pub fn run() {
         .plugin(tauri_plugin_google_signin::init())
         .setup(|app| {
             let scope = app.fs_scope();
-            scope.allow_directory("entry-storage", true);
+            scope.allow_file("entries.mpk");
 
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
-            load_entries,
-            save_today_entry,
-            load_today_entry
+            save_entry,
+            get_entry,
+            get_entries
         ])
         // context macro absolutely murders ide performance
         .run(context::context())
         .expect("error while running tauri application");
 }
 
-async fn handle_today_entry() -> JrnlIosResult<()> {
-    let tokio_file_handle = tokio::fs::File::open("entry-storage/today.mpk").await?;
-    let std_file_handle = tokio_file_handle.into_std().await;
-
-    let entry = rmp_serde::decode::from_read::<_, Entry>(std_file_handle)?;
-    let today = Local::now().date_naive();
-
-    // same day, we're okay to overwrite
-    if entry.date.eq(&today) {
-        return Ok(());
-    }
-
-    let mut existing_entries = inner_load_entries().await?;
-    existing_entries.insert(0, entry);
-
-    let tokio_file_handle = tokio::fs::File::create("entry-storage/entries.mpk").await?;
-    let mut std_file_handle = tokio_file_handle.into_std().await;
-
-    rmp_serde::encode::write(&mut std_file_handle, &existing_entries).map_err(Into::into)
-}
-
-async fn inner_load_entries() -> JrnlIosResult<Vec<Entry>> {
-    let tokio_file_handle = tokio::fs::File::open("entry-storage/entries.mpk").await?;
+async fn load_entries() -> JrnlIosResult<Vec<Entry>> {
+    let tokio_file_handle = tokio::fs::File::open("entries.mpk").await?;
     let std_file_handle = tokio_file_handle.into_std().await;
 
     rmp_serde::decode::from_read(std_file_handle).map_err(Into::into)
 }
 
 #[tauri::command]
-async fn save_today_entry(entry: Entry) -> Result<(), JrnlIosError> {
-    // if current cache is from yesterday, save to entries list
-    let _ = handle_today_entry().await;
-
-    let tokio_file_handle = tokio::fs::File::create("entry-storage/today.mpk").await?;
-    let mut std_file_handle = tokio_file_handle.into_std().await;
-
-    rmp_serde::encode::write(&mut std_file_handle, &entry).map_err(Into::into)
+async fn save_entry(entry: Entry) -> Result<(), JrnlIosError> {
+    todo!("this should save to entries in the correct position")
 }
 
 #[tauri::command]
-async fn load_today_entry() -> Option<Entry> {
-    let tokio_file_handle = tokio::fs::File::open("entry-storage/today.mpk")
-        .await
-        .ok()?;
-    let std_file_handle = tokio_file_handle.into_std().await;
-
-    rmp_serde::decode::from_read(std_file_handle).ok()
+async fn get_entry(id: String) -> JrnlIosResult<Option<Entry>> {
+    Ok(
+        load_entries().await?
+        .into_iter()
+        .find(|entry| entry.id == id)
+    )
 }
 
 #[tauri::command]
-async fn load_entries() -> JrnlIosResult<Vec<Entry>> {
-    inner_load_entries().await
+async fn get_entries() -> JrnlIosResult<Vec<Entry>> {
+    load_entries().await
 }
